7030022042028edbc659648933bf83b034448dff
SHAURYA MATHUR <mathurshaurya7@gmail.com>
2025-06-17 15:56:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from kafka import KafkaConsumer
 import json
 import joblib
 import pandas as pd
+import time
 
 # Load ML model
 model = joblib.load("logistic_model.pkl")
 
 # Set page config
 st.set_page_config(page_title="CardGaurd", layout="wide")
-st.title("🚨 Real-time Fraud Detection Dashboard : CARD GAURD")
+st.title("🚨 Real-time Fraud Detection Dashboard : CARD GAURD ")
 
 # Initialize Kafka Consumer
 consumer = KafkaConsumer(
@@ -20,23 +21,35 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-# Poll for the latest message
-message_pack = consumer.poll(timeout_ms=3000, max_records=1)
-latest_transaction = None
+# Create placeholder to update continuously
+placeholder = st.empty()
 
-for tp, messages in message_pack.items():
-    for message in messages:
-        latest_transaction = message.value
+try:
+    while True:
+        message_pack = consumer.poll(timeout_ms=3000, max_records=1)
+        latest_transaction = None
 
-# Display transaction and prediction
-if latest_transaction:
-    st.subheader("Latest Transaction:")
-    st.json(latest_transaction)
+        for tp, messages in message_pack.items():
+            for message in messages:
+                latest_transaction = message.value
 
-    features = pd.DataFrame([latest_transaction])
-    prediction = model.predict(features)[0]
-    result = "Fraud" if prediction == 1 else "Legit"
+        if latest_transaction:
+            features = pd.DataFrame([latest_transaction])
+            prediction = model.predict(features)[0]
+            result = "🛑 Fraud" if prediction == 1 else "✅ Legit"
 
-    st.markdown(f"### 🧠 Prediction: **{result}**")
-else:
-    st.warning("No new message received.")
+            with placeholder.container():
+                st.subheader("Latest Transaction:")
+                st.json(latest_transaction)
+
+                st.markdown(f"### 🧠 Prediction: **{result}**")
+                st.info("Updating in real time...")
+
+        else:
+            with placeholder.container():
+                st.warning("Waiting for new messages...")
+
+        time.sleep(3)
+
+except KeyboardInterrupt:
+    st.write("Stopped.")
